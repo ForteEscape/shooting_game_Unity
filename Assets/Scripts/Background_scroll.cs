81417c2311f636f08b8660b229f35acc5b1a7601
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-01 15:09:07 +0900

    add function
    add background moving

---
+++ Assets/Scripts/Background_scroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_scroll : MonoBehaviour {
+    public GameObject BackgroundUnderLayout;
+
+    public float scroll_velocity = 0.1f;
+    private float bgULDiry = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bgULDiry += Time.deltaTime * scroll_velocity;
+
+        BackgroundUnderLayout.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, bgULDiry);
+	}
+}
