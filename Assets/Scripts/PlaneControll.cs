3d01a07f2ac6f05be9641647049e698910cfe328
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-01 21:38:08 +0900

    add function
    add flight move function

---
+++ Assets/Scripts/PlaneControll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControll : MonoBehaviour {
+    public float FlightMoveSpeed = 250.0f;
+    public float RotateAngle = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCtrl();
+	}
+
+    void MoveCtrl()
+    {
+        float astMove = FlightMoveSpeed * Time.deltaTime;
+
+        float moveX = astMove * Input.GetAxis("Horizontal");
+        float moveY = astMove * Input.GetAxis("Vertical");
+        float rotateX = Input.GetAxis("Horizontal");
+
+        transform.Translate(moveX, moveY, 0, Space.World);
+        transform.eulerAngles = new Vector3(0, -rotateX * RotateAngle, 0);
+    }
+}
