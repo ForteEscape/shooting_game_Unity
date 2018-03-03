6461a508bd6644e5c39bfa13358b33a5f1c7c4f2
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-03 23:24:39 +0900

    add UI
    add basic UI : Title UI and Setting UI

---
+++ Assets/Scripts/Btn_Setting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Btn_Setting : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+      
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (this.GetComponentInChildren<Text>().text == "Btn_Setting" && Input.GetKey(KeyCode.Z)) 
+        {
+            Application.LoadLevel("Setting");
+        }
+	}
+}
