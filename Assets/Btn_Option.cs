ef296b575814e8a481dcba39eb915b75dbf8a7bb
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-04 20:56:37 +0900

    add UI
    add Title UI and scene change function

--- Assets/Btn_Option.cs
+++ Assets/Btn_Option.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Btn_Quit : MonoBehaviour {
+public class Btn_Option : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +11,6 @@ public class Btn_Quit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.Z))
-        {
-            print("Quit!!!!!");
-            Application.Quit();
-        }
+		
 	}
 }
---
+++ Assets/Btn_Start.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class Btn_Start : MonoBehaviour {
+
+	public void changeScene()
+    {
+        Application.LoadLevel("inGame");
+    }
+}
