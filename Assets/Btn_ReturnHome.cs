b1434ed6feb6a6be4ac195f87e98c20537614fe5
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-04 21:28:03 +0900

    add UI
    add Option UI

---
+++ Assets/Btn_ReturnHome.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Btn_ReturnHome : MonoBehaviour {
+
+	public void Return()
+    {
+        Application.LoadLevel("GameTitle");
+    }
+}
