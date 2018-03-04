f08ccaafbe5807c193e606ee47a93bfbd33bfb49
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-04 23:21:52 +0900

    add UI
    merge Option scene and GameTitle scene in one scene

---
+++ Assets/Scripts/UI/SettingManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingManager : MonoBehaviour {
+
+    public GameObject mainMenuHolder;
+    public GameObject optionMenuHolder;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("inGame");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuHolder.SetActive(false);
+        optionMenuHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionMenuHolder.SetActive(false);
+    }
+
+	public void SetScreenResolution(int i)
+    {
+
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+
+    }
+
+    public void SetMusicVolume()
+    {
+
+    }
+}
--- Assets/toggle_spriteChange.cs
+++ Assets/toggle_spriteChange.cs
@@ -1,7 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class toggle_spriteChange : MonoBehaviour {
+    public Sprite switchOn;
+    public Sprite switchOff;
+    
+    public void OnChangeValue()
+    {
+        bool onoffSwitch = gameObject.GetComponent<Toggle>().isOn;
 
+        if (onoffSwitch)
+        {
+            GetComponentInChildren<Image>().sprite = switchOn;
+        }
+        else
+        {
+            GetComponentInChildren<Image>().sprite = switchOff;
+        }
+    }
 }
