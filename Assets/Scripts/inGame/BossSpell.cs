7c5b4a9e157a6807bc1c315fe1c25d21e82afd74
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-07 19:43:52 +0900

    add function
    add bullet patten function
    delete plugIn --> I'll make myself

---
+++ Assets/Scripts/inGame/BossSpell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpell : MonoBehaviour {
+
+    public int ShotPerOnce;
+    public GameObject boss;
+    public GameObject bullet;
+    public float speed;
+    public float bulletCoolTime = 1f;
+
+    IEnumerator SpellStart()
+    {
+        float angle = 360 / ShotPerOnce;
+
+        do
+        {
+            for (int i = 0; i < ShotPerOnce; i++)
+            {
+                Debug.Log(i);
+                GameObject obj;
+
+                obj = (GameObject)Instantiate(bullet, boss.transform.position, Quaternion.identity);
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed * Mathf.Cos(Mathf.PI * 2 * i / ShotPerOnce), speed * Mathf.Sin(Mathf.PI * i * 2 / ShotPerOnce)));
+
+                obj.transform.Rotate(new Vector3(0f, 0f, 360 * i / ShotPerOnce - 90));
+            }
+            yield return new WaitForSeconds(bulletCoolTime);
+        } while (true);
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpellStart());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
