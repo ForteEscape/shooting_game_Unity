decc72eaeb6532e11b5f37a92f11e89ba3c4469c
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-08 19:49:01 +0900

    add boss pattern
    #9

--- Assets/Scripts/inGame/bossTest.cs
+++ Assets/Scripts/inGame/bossTest.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class bossTest : MonoBehaviour {
 
-    public GameObject bulletPrefab;
-
-    public int shotPerOnce;
-
-    public float bulletSpeed;
+    public int ShotPerOnce;
+    public GameObject boss;
+    public GameObject bullet;
+    public float speed;
     public float bulletCoolTime = 1f;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(BulletGenerator());
-	}
+    public float testindex_Y = 2;
+    public float testindex_X = 2;
 
-    IEnumerator BulletGenerator()
+    IEnumerator SpellStart()
     {
-        yield return new WaitForSeconds(bulletCoolTime);
+        float angle = 360 / ShotPerOnce;
+
+        do
+        {
+            for (int i = 0; i < ShotPerOnce; i++)
+            {
+                //Debug.Log(i);
+                GameObject obj;
+
+                obj = (GameObject)Instantiate(bullet, boss.transform.position, Quaternion.identity);
+                obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed * Mathf.Cos(Mathf.PI * testindex_X * i / ShotPerOnce), speed * Mathf.Sin(Mathf.PI * i * testindex_Y / ShotPerOnce)));
+
+                obj.transform.Rotate(new Vector3(0f, 0f, 360 * i / ShotPerOnce - 90));
+            }
+            yield return new WaitForSeconds(bulletCoolTime);
+        } while (true);
     }
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpellStart());
+	}
 	
 	// Update is called once per frame
 	void Update () {
